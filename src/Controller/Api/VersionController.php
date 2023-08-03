19288ed83e4be81a026a11769d7a580f0d2e568b
Oleksii Panfilov <oleksii.panfilov@overdose.digital>
2023-08-03 13:20:35 +0300

    Creating module version endpoint https://www.wrike.com/open.htm?id=1169034131

---
+++ src/Controller/Api/VersionController.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace Klaviyo\Integration\Controller\Api;
+
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Klaviyo\Integration\klavi_overd;
+use Shopware\Core\Framework\Plugin\PluginEntity;
+
+#[Route(defaults: ['_routeScope' => ['api']])]
+class VersionController extends AbstractController
+{
+    private $pluginRepository;
+
+    public function __construct(
+        EntityRepository $pluginRepository
+    ) {
+        $this->pluginRepository = $pluginRepository;
+    }
+
+
+    #[Route(path:"/api/klaviyo/version", name:"api.klaviyo.version", defaults: ['auth_required' => false], methods:["get"])]
+    public function version(Context $context): JsonResponse
+    {
+        $criteria = new Criteria();
+        $criteria->addFilter(new EqualsFilter('baseClass', klavi_overd::class));
+
+        /** @var PluginEntity|null $plugin */
+        $plugin = $this->pluginRepository->search($criteria, $context)->first();
+
+        if ($plugin === null) {
+            return new JsonResponse(['Klaviyo plugin was not found'], Response::HTTP_OK);
+        }
+
+        $pluginVersion = $plugin->getVersion();
+
+        return new JsonResponse(['Current Klaviyo Version' => $pluginVersion], Response::HTTP_OK);
+    }
+}
