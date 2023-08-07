3d02cad477397b88b813af54b20d856d9a46f2cb
Oleksii Panfilov <oleksii.panfilov@overdose.digital>
2023-08-07 22:59:59 +0300

    Added guest redirect to confirm page if order data are present https://www.wrike.com/open.htm?id=1148559526

--- src/Controller/Storefront/CartController.php
+++ src/Controller/Storefront/CartController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Shopware\Core\Checkout\Customer\SalesChannel\AbstractRegisterRoute;
+use Psr\Log\LoggerInterface;
 
 /**
  * @Route(defaults={"_routeScope"={"storefront"}})
