6430dc11a8e68f5d28bf09c3d93e2d7ae7695d11
Oleksii Panfilov <oleksii.panfilov@overdose.digital>
2023-10-19 19:25:20 +0300

    Added validation to public key API https://www.wrike.com/open.htm?id=1231401519

--- src/Klaviyo/Client/Serializer/SerializerFactory.php
+++ src/Klaviyo/Client/Serializer/SerializerFactory.php
@@ -7,12 +7,14 @@ use Klaviyo\Integration\Klaviyo\Client\ApiTransfer\Message\EventTracking\OrderEv
 use Klaviyo\Integration\Klaviyo\Client\Configuration\ConfigurationInterface;
 use Klaviyo\Integration\Klaviyo\Client\Serializer\Denormalizer\AddProfilesToListResponseDenormalizer;
 use Klaviyo\Integration\Klaviyo\Client\Serializer\Denormalizer\CollectionDenormalizer;
+use Klaviyo\Integration\Klaviyo\Client\Serializer\Denormalizer\GetAccountDenormalizer;
 use Klaviyo\Integration\Klaviyo\Client\Serializer\Denormalizer\GetExcludedSubscribersResponseDenormalizer;
 use Klaviyo\Integration\Klaviyo\Client\Serializer\Denormalizer\GetListProfilesResponseDenormalizer;
 use Klaviyo\Integration\Klaviyo\Client\Serializer\Denormalizer\GetProfileIdResponseDenormalizer;
 use Klaviyo\Integration\Klaviyo\Client\Serializer\Denormalizer\GetProfilesListsResponseDenormalizer;
 use Klaviyo\Integration\Klaviyo\Client\Serializer\Denormalizer\ProfileInfoDenormalizer;
 use Klaviyo\Integration\Klaviyo\Client\Serializer\Denormalizer\RemoveProfilesFromListResponseDenormalizer;
+use Klaviyo\Integration\Klaviyo\Client\Serializer\Denormalizer\SubscribeToListResponseDenormalizer;
 use Klaviyo\Integration\Klaviyo\Client\Serializer\Denormalizer\UpdateProfileResponseDenormalizer;
 use Klaviyo\Integration\Klaviyo\Client\Serializer\Normalizer\AddedToCartEventTrackingRequestNormalizer;
 use Klaviyo\Integration\Klaviyo\Client\Serializer\Normalizer\AddProfilesToListRequestsNormalizer;
@@ -23,15 +25,14 @@ use Klaviyo\Integration\Klaviyo\Client\Serializer\Normalizer\CustomerPropertiesN
 use Klaviyo\Integration\Klaviyo\Client\Serializer\Normalizer\IdentifyProfileRequestNormalizer;
 use Klaviyo\Integration\Klaviyo\Client\Serializer\Normalizer\OrderedProductEventTrackingRequestNormalizer;
 use Klaviyo\Integration\Klaviyo\Client\Serializer\Normalizer\PaidOrderEventTrackingRequestNormalizer;
-use Klaviyo\Integration\Klaviyo\Client\Serializer\Normalizer\ShippedOrderEventTrackingRequestNormalizer;
 use Klaviyo\Integration\Klaviyo\Client\Serializer\Normalizer\RefundedOrderEventTrackingRequestNormalizer;
 use Klaviyo\Integration\Klaviyo\Client\Serializer\Normalizer\RemoveProfilesFromListRequestNormalizer;
+use Klaviyo\Integration\Klaviyo\Client\Serializer\Normalizer\ShippedOrderEventTrackingRequestNormalizer;
+use Klaviyo\Integration\Klaviyo\Client\Serializer\Normalizer\StartedCheckoutEventTrackingRequestNormalizer;
 use Klaviyo\Integration\Klaviyo\Client\Serializer\Normalizer\SubscribeToListRequestNormalizer;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
-use Klaviyo\Integration\Klaviyo\Client\Serializer\Denormalizer\SubscribeToListResponseDenormalizer;
-use Klaviyo\Integration\Klaviyo\Client\Serializer\Normalizer\StartedCheckoutEventTrackingRequestNormalizer;
 
 class SerializerFactory
 {
@@ -72,6 +73,7 @@ class SerializerFactory
                 new GetProfileIdResponseDenormalizer(),
                 new UpdateProfileResponseDenormalizer(),
                 new StartedCheckoutEventTrackingRequestNormalizer($configuration),
+                new GetAccountDenormalizer(),
             ],
             [
                 new JsonEncoder(),
