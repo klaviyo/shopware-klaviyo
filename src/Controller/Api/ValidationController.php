04402d42f39cfc3f9d01b417e38251c1cbafa156
Oleksii Panfilov <oleksii.panfilov@overdose.digital>
2023-10-23 22:20:36 +0300

    Added selector the "Klaviyo List name for subscribers sync" field https://www.wrike.com/open.htm?id=1235327817

--- src/Controller/Api/ValidationController.php
+++ src/Controller/Api/ValidationController.php
@@ -13,10 +13,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Shopware\Core\Framework\Routing\Annotation\RouteScope;
 
 /**
- * @RouteScope(scopes={"api"})
+ * @Route(defaults={"_routeScope"={"api"}})
  */
 class ValidationController extends AbstractController
 {
