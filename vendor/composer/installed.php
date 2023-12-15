fbdfc6cb8f7c8a206f8c2fa44795662ae29fceae
Rafael <rafayel.sargsyan@overdose.digital>
2023-12-15 18:02:52 +0400

    Updated scheduler version to 1.0.5

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'od/sw6-klaviyo-integration',
-        'pretty_version' => '1.0.9',
-        'version' => '1.0.9.0',
+        'pretty_version' => '1.6.0',
+        'version' => '1.6.0.0',
         'reference' => NULL,
         'type' => 'shopware-platform-plugin',
         'install_path' => __DIR__ . '/../../',
@@ -11,17 +11,17 @@
     ),
     'versions' => array(
         'od/sw6-job-scheduler' => array(
-            'pretty_version' => '1.0.3',
-            'version' => '1.0.3.0',
-            'reference' => '86f681957e8680fc74389c02eeccef3a04e45714',
+            'pretty_version' => '1.0.5',
+            'version' => '1.0.5.0',
+            'reference' => 'c97d38a0d15fab551800c17a189cd85474efdc0a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../od/sw6-job-scheduler',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'od/sw6-klaviyo-integration' => array(
-            'pretty_version' => '1.0.9',
-            'version' => '1.0.9.0',
+            'pretty_version' => '1.6.0',
+            'version' => '1.6.0.0',
             'reference' => NULL,
             'type' => 'shopware-platform-plugin',
             'install_path' => __DIR__ . '/../../',
