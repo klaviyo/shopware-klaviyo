762d4155a127bc968d02c845506fd1a3d256907d
roman.h <roman.h@overdose.digital>
2024-01-16 17:47:20 +0200

    1277792047-multilingual_product_url_is_incorrect-64 Fix for issue with productUrl having wrong domain in their path https://www.wrike.com/open.htm?id=1277792047

--- src/Klaviyo/Gateway/KlaviyoGateway.php
+++ src/Klaviyo/Gateway/KlaviyoGateway.php
@@ -104,7 +104,6 @@ class KlaviyoGateway
     {
         $result = new OrderTrackingResult();
         $requestOrderIdMap = $requests = [];
-
         /** @var OrderEventInterface $event */
         foreach ($orderEvents as $event) {
             foreach ($event->getOrder()->getLineItems() ?? [] as $lineItem) {
@@ -114,7 +113,7 @@ class KlaviyoGateway
 
                 try {
                     $request = $this->productEventTranslator
-                        ->translateToOrderedProductEventRequest($context, $lineItem, $event->getOrder());
+                        ->translateToOrderedProductEventRequest($context, $lineItem, $event->getOrder(), $event->getOrder()->getLanguageId());
                     $requestOrderIdMap[spl_object_id($request)] = $event->getOrder()->getId();
                     $requests[] = $request;
                 } catch (\Throwable $e) {
