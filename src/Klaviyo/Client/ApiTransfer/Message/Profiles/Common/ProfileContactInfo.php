93f3fc73904bec55044c19723588beadbbd8552a
Oleksii Panfilov <oleksii.panfilov@overdose.digital>
2024-02-29 17:41:07 +0200

    Added additional customer data to the customer profile sync https://www.wrike.com/open.htm?id=1305830290

--- src/Klaviyo/Client/ApiTransfer/Message/Profiles/Common/ProfileContactInfo.php
+++ src/Klaviyo/Client/ApiTransfer/Message/Profiles/Common/ProfileContactInfo.php
@@ -6,11 +6,22 @@ class ProfileContactInfo
 {
     private string $customerId;
     private ?string $email;
+    private ?string $firstname;
+    private ?string $lastname;
+    private ?string $salutation;
 
-    public function __construct(string $customerId, string $email = null)
-    {
+    public function __construct(
+        string $customerId,
+        string $email = null,
+        string $firstname = null,
+        string $lastname = null,
+        string $salutation = null
+    ) {
         $this->customerId = $customerId;
         $this->email = $email;
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $this->salutation = $salutation;
     }
 
     public function getCustomerId(): ?string
@@ -21,5 +32,20 @@ class ProfileContactInfo
     public function getEmail(): ?string
     {
         return $this->email;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function getSalutation(): ?string
+    {
+        return $this->salutation;
     }
 }
