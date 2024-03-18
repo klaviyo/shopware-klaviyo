bbb9973bb0cfa37ec5409be8955674ccd8f351b8
Koblia Serhii <sergiy.k@overdose.digital>
2024-03-18 17:20:27 +0200

    [1318913728] Changelog:
    Changed:
    
    - getting locale code flow in case customer is null
    
    Task:
    
    - https://www.wrike.com/open.htm?id=1318913728

--- src/Klaviyo/Gateway/Translator/CustomerPropertiesTranslator.php
+++ src/Klaviyo/Gateway/Translator/CustomerPropertiesTranslator.php
@@ -39,6 +39,10 @@ class CustomerPropertiesTranslator
     }
 
     /**
+     * @param Context $context
+     * @param OrderEntity $orderEntity
+     *
+     * @return CustomerProperties
      * @throws JobRuntimeWarningException
      */
     public function translateOrder(Context $context, OrderEntity $orderEntity): CustomerProperties
@@ -62,9 +66,7 @@ class CustomerPropertiesTranslator
         $country = $this->addressHelper->getAddressCountry($context, $customerAddress);
 
         $customFields = $this->prepareCustomFields($customer, $orderEntity->getSalesChannelId());
-        $birthday = $customer ? $customer->getBirthday() : null;
-
-        $localeCode = $this->localeCodeProducer->getLocaleCodeFromContext($customer->getLanguageId(), $context);
+        $birthday = $customer?->getBirthday();
 
         return new CustomerProperties(
             $customer ? $customer->getEmail() : $orderCustomer->getEmail(),
@@ -91,8 +93,30 @@ class CustomerPropertiesTranslator
                 $customer->getBoundSalesChannel(),
                 $context
             ) : null,
-            $localeCode ?: null
+            $this->getLocaleCode($orderEntity, $context)
         );
+    }
+
+    /**
+     * Get locale code.
+     *
+     * @param OrderEntity $orderEntity
+     * @param Context $context
+     *
+     * @return string|null
+     */
+    private function getLocaleCode(OrderEntity $orderEntity, Context $context): ?string
+    {
+        try {
+            $orderCustomer = $orderEntity->getOrderCustomer()->getCustomer();
+
+            return $this->localeCodeProducer->getLocaleCodeFromContext(
+                $orderCustomer ? $orderCustomer->getLanguageId() : $orderEntity->getLanguageId(),
+                $context
+            );
+        } catch (\Exception $e) {
+            return null;
+        }
     }
 
     private function guessRelevantCustomerAddress(?CustomerEntity $customerEntity): ?CustomerAddressEntity
