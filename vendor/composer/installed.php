7e992e08d2b231912ce6b46ea1db5180491ebf62
Oleksii Panfilov <oleksii.panfilov@overdose.digital>
2024-03-22 18:06:42 +0200

    Scheduler update to version 2.0.8

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,12 +11,12 @@
     ),
     'versions' => array(
         'od/sw6-job-scheduler' => array(
-            'pretty_version' => '2.0.7',
-            'version' => '2.0.7.0',
+            'pretty_version' => '2.0.8',
+            'version' => '2.0.8.0',
+            'reference' => 'fc5441ec481ba81675ce814e9ca7d7ed00b7dcbf',
             'type' => 'library',
             'install_path' => __DIR__ . '/../od/sw6-job-scheduler',
             'aliases' => array(),
-            'reference' => '942ea8e0a3e3c94bc32b897e02d3927c35ff7e19',
             'dev_requirement' => false,
         ),
         'od/sw6-klaviyo-integration' => array(
