cc42093c1acd48e49d915a7ec86af0582f69da1a
Koblia Serhii <sergiy.k@overdose.digital>
2024-03-25 09:52:00 +0200

    [1325721792] Changelog:
    Changed:
    
    - added additional checks
    
    Task: https://www.wrike.com/open.htm?id=1325721792

--- src/Model/UseCase/ScheduleBackgroundJob.php
+++ src/Model/UseCase/ScheduleBackgroundJob.php
@@ -163,6 +163,9 @@ class ScheduleBackgroundJob
         foreach ($channelIds as $channelId) {
             try {
                 foreach ($this->excludedSubscribersProvider->getExcludedSubscribers($channelId) as $result) {
+                    if (!count($result->getEmails())) {
+                        continue;
+                    }
                     $jobMessage = new Message\ExcludedSubscriberSyncMessage(
                         Uuid::randomHex(),
                         $parentJobId,
