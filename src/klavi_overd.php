6794a2c1bf3dfc31a21fc480536625f8f269cc1f
admin <nikita.yagzhen@overdose.digital>
2024-09-20 20:52:37 +0300

    #build

--- src/klavi_overd.php
+++ src/klavi_overd.php
@@ -3,16 +3,42 @@
 namespace Klaviyo\Integration;
 
 use Doctrine\DBAL\Connection;
+use Klaviyo\Integration\Utils\Lifecycle;
 use Klaviyo\Integration\Utils\Lifecycle\Update\UpdateOldTemplate;
 use Klaviyo\Integration\Utils\Lifecycle\Update\UpdateTo105;
+use Klaviyo\Integration\Utils\MigrationHelper;
 use League\Flysystem\Filesystem;
 use League\Flysystem\Local\LocalFilesystemAdapter;
+use Od\Scheduler\OdScheduler;
+use Shopware\Core\Framework\Parameter\AdditionalBundleParameters;
 use Shopware\Core\Framework\Plugin;
-use Shopware\Core\Framework\Plugin\Context\UpdateContext;
+use Shopware\Core\Framework\Plugin\Context\{ActivateContext, UninstallContext, UpdateContext};
+use Shopware\Core\Framework\Plugin\Util\AssetService;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Loader\DelegatingLoader;
+use Symfony\Component\Config\Loader\LoaderResolver;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\DirectoryLoader;
+use Symfony\Component\DependencyInjection\Loader\GlobFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class klavi_overd extends Plugin
 {
+    public function activate(ActivateContext $activateContext): void
+    {
+        parent::activate($activateContext);
+        /** @var AssetService $assetService */
+        $assetService = $this->container->get('klaviyo.plugin.assetservice.public');
+        /** @var MigrationHelper $migrationHelper */
+        $migrationHelper = $this->container->get(MigrationHelper::class);
+
+        foreach ($this->getDependencyBundles() as $bundle) {
+            $migrationHelper->getMigrationCollection($bundle)->migrateInPlace();
+            $assetService->copyAssetsFromBundle((new \ReflectionClass($bundle))->getShortName());
+        }
+    }
+
     public function update(UpdateContext $updateContext): void
     {
         if (\version_compare($updateContext->getCurrentPluginVersion(), '1.0.5', '<=')) {
@@ -32,8 +58,47 @@ class klavi_overd extends Plugin
         parent::update($updateContext);
     }
 
+    public function uninstall(UninstallContext $uninstallContext): void
+    {
+        if ($uninstallContext->keepUserData()) {
+            return;
+        }
+
+        (new Lifecycle($this->container, true))->uninstall($uninstallContext);
+    }
+
+    public function getAdditionalBundles(AdditionalBundleParameters $parameters): array
+    {
+        return $this->getDependencyBundles();
+    }
+
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $locator = new FileLocator('Resources/config');
+
+        $resolver = new LoaderResolver([
+            new YamlFileLoader($container, $locator),
+            new GlobFileLoader($container, $locator),
+            new DirectoryLoader($container, $locator),
+        ]);
+
+        $configLoader = new DelegatingLoader($resolver);
+
+        $confDir = \rtrim($this->getPath(), '/') . '/Resources/config';
+        $configLoader->load($confDir . '/{packages}/*.yaml', 'glob');
+    }
+
+    private function getDependencyBundles(): array
+    {
+        return [
+            new OdScheduler(),
+        ];
+    }
+
     public function executeComposerCommands(): bool
     {
-        return true;
+        return false;
     }
 }
