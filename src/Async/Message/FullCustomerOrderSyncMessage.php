26ec5fa76c83cce644689bb242ca206a465ae9a7
Oleksii Panfilov <oleksii.panfilov@overdose.digital>
2024-11-05 22:38:19 +0200

    Added customer syncs  https://www.wrike.com/open.htm?id=1517943592

---
+++ src/Async/Message/FullCustomerOrderSyncMessage.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace Klaviyo\Integration\Async\Message;
+
+use Klaviyo\Integration\Model\UseCase\Operation\FullCustomerOrderSyncOperation;
+use Shopware\Core\Framework\Context;
+
+class FullCustomerOrderSyncMessage extends AbstractBasicMessage
+{
+    protected static string $defaultName = 'Full Customer Sync Operation';
+
+    private int $offset;
+
+    public function __construct(
+        string   $jobId,
+        ?string  $name = null,
+        ?Context $context = null,
+        int      $offset = 0
+    ) {
+        parent::__construct($jobId, $name, $context);
+        $this->offset = $offset;
+    }
+    public function getHandlerCode(): string
+    {
+        return FullCustomerOrderSyncOperation::OPERATION_HANDLER_CODE;
+    }
+
+    public function getOffset(): int
+    {
+        return $this->offset;
+    }
+}
