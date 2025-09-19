7846c843c579966f1c6eec5c5187db1dffb267b5
dbyvalin <dimy@h1.nl>
2025-09-19 09:10:19 +0300

    [KLAVIYO-155] klaviyo sw plugin version tracking via headers

--- src/Resources/app/storefront/src/klaviyo/plugins/kaviyo-back-in-stock-notification.plugin.js
+++ src/Resources/app/storefront/src/klaviyo/plugins/kaviyo-back-in-stock-notification.plugin.js
@@ -17,7 +17,9 @@ export default class KlaviyoBackInStockNotification extends Plugin {
         errorVariantMessageSelector: '.klaviyo-error-variant',
         notValidEmailMessageSelector: '.klaviyo-email-not-valid',
         fetchHeaderAccept: "application/json",
-        version: "unknown"
+        composerPluginVersion: null,
+        dbPluginVersion: null,
+        shopwareVersion: null
     };
 
     init() {
@@ -34,8 +36,12 @@ export default class KlaviyoBackInStockNotification extends Plugin {
         this._errorMessage = DomAccess.querySelector(this.el, this.options.errorMessageSelector);
         this._emailNotValid = DomAccess.querySelector(this.el, this.options.notValidEmailMessageSelector);
         this._errorVariantMessage = DomAccess.querySelector(this.el, this.options.errorVariantMessageSelector);
-        const versionEl = document.querySelector('[data-klaviyo-plugin-version]');
-        this.options.version = versionEl ? versionEl.getAttribute('data-klaviyo-plugin-version') : this.options.version;
+        const composerPluginVersionEl = document.querySelector('[data-klaviyo-plugin-version]');
+        const dbPluginVersionEl = document.querySelector('[data-klaviyo-plugin-version-db]');
+        const shopwareVersionEl = document.querySelector('[data-klaviyo-shopware-version]');
+        this.options.composerPluginVersion = composerPluginVersionEl ? composerPluginVersionEl.getAttribute('data-klaviyo-plugin-version') : "unknown";
+        this.options.dbPluginVersion = dbPluginVersionEl ? dbPluginVersionEl.getAttribute('data-klaviyo-plugin-version-db') : "unknown";
+        this.options.shopwareVersion = shopwareVersionEl ? shopwareVersionEl.getAttribute('data-klaviyo-shopware-version') : "unknown";
     }
 
     registerEvents() {
@@ -92,7 +98,9 @@ export default class KlaviyoBackInStockNotification extends Plugin {
                 "accept": this.options.fetchHeaderAccept,
                 "content-type": this.options.contentType,
                 "revision": this.options.revision,
-                "x-sw-plugin-version": this.options.version
+                "x-sw-plugin-version": this.options.composerPluginVersion,
+                "x-sw-plugin-version-db": this.options.dbPluginVersion,
+                "x-sw-version": this.options.shopwareVersion,
             },
             "body": body,
             "method": "POST",
