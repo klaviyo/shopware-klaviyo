1504ba28fc0fd93d6bb0f51c419e82616526decf
eric2o13 <eric@h1.nl>
2026-04-02 15:35:14 +0200

    [KLAVIYO-184] Update API Revision

--- src/Klaviyo/Gateway/ClientConfigurationFactory.php
+++ src/Klaviyo/Gateway/ClientConfigurationFactory.php
@@ -10,7 +10,7 @@ use Klaviyo\Integration\Klaviyo\Client\Configuration\{Configuration, Configurati
 class ClientConfigurationFactory
 {
     public const AUTHORIZATION_PREKEY = 'Klaviyo-API-Key';
-    public const API_REVISION_DATE = '2025-10-15';
+    public const API_REVISION_DATE = '2026-01-15';
     private const TRACKING_ENDPOINT_URL = 'https://a.klaviyo.com/api/events';
     private const IDENTIFY_ENDPOINT_URL = 'https://a.klaviyo.com/api/profile-import';
     private const GLOBAL_NEW_ENDPOINT_URL = 'https://a.klaviyo.com/api';
